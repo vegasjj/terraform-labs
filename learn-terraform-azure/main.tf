c0047a0d441d1a88077f692cc0240b5d659949ff
Jose Javer Vegas <vegasjj@gmail.com>
2025-08-05 23:23:56 -0400

    Add Terraform change lab for Azure

--- learn-terraform-azure/main.tf
+++ learn-terraform-azure/main.tf
@@ -17,4 +17,17 @@ provider "azurerm" {
 resource "azurerm_resource_group" "rg" {
   name     = "myTFResourceGroup"
   location = "westus2"
+
+  tags = {
+    Environment = "Terraform Getting Started"
+    Team = "DevOps"
+  }
+}
+
+# Create a virtual network
+resource "azurerm_virtual_network" "vnet" {
+  name                = "myTFVnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = "westus2"
+  resource_group_name = azurerm_resource_group.rg.name
 }
